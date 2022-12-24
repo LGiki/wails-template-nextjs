45cda8c07f21cbc3298f15dc2910725f677d555a
Alex DeFreese <alexdefreese@gmail.com>
2022-12-24 17:07:09 -0500

    Embed frontend with all syntax
    Changes the `assets` embed to use `all:frontend/out` instead of `frontend/out/*` to prevent the error described in #1. Fix recommended by @siddmo.

--- main.tmpl.go
+++ main.tmpl.go
@@ -11,7 +11,7 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
-//go:embed frontend/out/* frontend/out/_next/static/*/* frontend/out/_next/static/*/*/*
+//go:embed all:frontend/out frontend/out/_next/static/*/* frontend/out/_next/static/*/*/*
 var assets embed.FS
 
 //go:embed build/appicon.png
